11e257826c7ae3a799381a26c780373967dec389
Andreyrvs <cortaesai@gmail.com>
2022-01-19 15:32:39 -0300

    Requisito: 02 - Adiciona componente LintagemDeProdutos

---
+++ src/components/ListagemDeProdutos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListagemDeProdutos extends Component {
+  render() {
+    return (
+      <div>
+        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      </div>
+    );
+  }
+}
+
+export default ListagemDeProdutos;
