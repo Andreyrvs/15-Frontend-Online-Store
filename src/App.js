c0280accf361778a4389ab433f3ce935acc9db94
Elias Paiva <elias.jpaiva@gmail.com>
2022-01-20 13:24:02 +0100

    requesito 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ListagemDeProdutos from './components/ListagemDeProdutos';
+import ShoppingCart from './components/ShoppingCart';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ ListagemDeProdutos } />
+          <Route path="/ShoppingCart" component={ ShoppingCart } />
         </Switch>
       </BrowserRouter>
     </div>
