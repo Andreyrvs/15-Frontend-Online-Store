79d1fadfca75a68804f906f41e16d39776f162f8
Andreyrvs <cortaesai@gmail.com>
2022-01-21 20:19:24 -0300

    Adiciona arquivo de estilo para pagina ShoppingCart.js

--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class ShoppingCart extends Component {
   render() {
     return (
-      <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      </div>
+      <section className="page-container">
+        <div>
+          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        </div>
+      </section>
     );
   }
 }
