0a72c78f9ee084c4e68a481a07bf2744043c8344
Elias Paiva <elias.jpaiva@gmail.com>
2022-01-25 23:47:42 +0100

    fixing lint

--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -27,18 +27,18 @@ class ShoppingCart extends Component {
     return (
       <>
         <div>
-          <p data-testid='shopping-cart-empty-message'>
+          <p data-testid="shopping-cart-empty-message">
             Seu carrinho está vazio
           </p>
         </div>
         <div>
           <ul>
             {getProduct.map((product) => (
-              <li key={product.title}>
-                <p>{product.id}</p>
-                <img src={product.thumbnail} alt={product.title} />
-                <p data-testid='shopping-cart-product-name'>{product.title}</p>
-                <p data-testid='shopping-cart-product-quantity'>1
+              <li key={ product.title }>
+                <p>{ product.id }</p>
+                <img src={ product.thumbnail } alt={ product.title } />
+                <p data-testid="shopping-cart-product-name">{ product.title }</p>
+                <p data-testid="shopping-cart-product-quantity">
                 </p>
               </li>
             ))}
