2e7260641b06b025153ef43ec174da39f803fdd6
Elias Paiva <elias.jpaiva@gmail.com>
2022-01-25 23:23:52 +0100

    requesito 9

--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      getProduct: [],
+    };
+    this.getFromLocalStorage = this.getFromLocalStorage.bind(this);
+  }
+
+  componentDidMount() {
+    this.getFromLocalStorage();
+  }
+
+  getFromLocalStorage() {
+    const arrayAntigo = localStorage.getItem('chave');
+    if (arrayAntigo !== null) {
+      this.setState(() => ({
+        getProduct: JSON.parse(arrayAntigo),
+      }));
+    }
+  }
+
   render() {
+    const { getProduct } = this.state;
     return (
-      <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      </div>
+      <>
+        <div>
+          <p data-testid='shopping-cart-empty-message'>
+            Seu carrinho está vazio
+          </p>
+        </div>
+        <div>
+          <ul>
+            {getProduct.map((product) => (
+              <li key={product.title}>
+                <p>{product.id}</p>
+                <img src={product.thumbnail} alt={product.title} />
+                <p data-testid='shopping-cart-product-name'>{product.title}</p>
+                <p data-testid='shopping-cart-product-quantity'>1
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
     );
   }
 }
