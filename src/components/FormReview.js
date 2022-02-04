c910a0fa1f45a282c1cca557863eed222e1e0db2
Andreyrvs <cortaesai@gmail.com>
2022-02-04 14:15:13 -0300

    Requisito: 11 - rendeniza os comentarios de cada item corretamente

--- src/components/FormReview.js
+++ src/components/FormReview.js
@@ -5,50 +5,24 @@ class FormReview extends React.Component {
   constructor() {
     super();
 
-    // this.state = {
-    //   score: 0,
-    //   email: '',
-    //   comments: '',
-    // };
-
     this.state = {
       reviewItems: JSON.parse(localStorage.getItem('reviewItems')) || [],
     };
   }
 
-  // saveReviews = (event) => {
-  //   event.preventDefault(event);
-  //   const {
-  //     email,
-  //     comments,
-  //   } = this.state;
-  //   const { product } = this.props;
-  //   const review = JSON.parse(localStorage.getItem('review')) || [];
-
-  //   review.push({
-  //     email: [...review, email],
-  //     comments: [...review, comments],
-  //     productId: product.id,
-  //   });
-
-  //   // review.push({
-  //   //   reviews: [...review, { email, comments, productId: product.id }],
-  //   // });
-
-  //   localStorage.setItem('review', JSON.stringify(review));
-  // }
-
   saveReviews = (event) => {
     event.preventDefault();
+    const { product } = this.props;
+
     this.setState((prevState) => ({
       reviewItems: [...prevState.reviewItems, {
         email: prevState.email,
         comments: prevState.comments,
+        productId: product.id,
       }],
     }), () => {
       const { reviewItems } = this.state;
       localStorage.setItem('reviewItems', JSON.stringify(reviewItems));
-      // window.location.reload();
     });
   }
 
@@ -60,26 +34,21 @@ class FormReview extends React.Component {
   }
 
   render() {
-    // const {
-    //   email,
-    //   comments,
-    // } = this.state;
     const { reviewItems } = this.state;
 
     const { product } = this.props;
-    // const items = JSON.parse(localStorage.getItem('reviews'));
-    // const reviewItems = items || [];
 
     const listReviews = (
-      reviewItems.map((review, index) => (
+      reviewItems.filter((reviewId) => reviewId.productId === product.id)
+        .map((review, index) => (
 
-        <div key={ index }>
-          <p>{ review.email }</p>
-          <p>{ review.score }</p>
-          <p>{ review.comments }</p>
-        </div>
+          <div key={ index }>
+            <p>{ review.email }</p>
+            <p>{ review.score }</p>
+            <p>{ review.comments }</p>
+          </div>
 
-      ))
+        ))
     );
 
     return (
